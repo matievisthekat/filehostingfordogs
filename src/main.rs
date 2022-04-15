80d9a92c8ad421bac0615df73d13185a531f0621
matievisthekat <matievisthekat@gmail.com>
2022-04-15 16:04:56 +0200

    encode file metadata in base64 as file name

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,11 @@ use rocket::data::{Data, ToByteUnit};
 use rocket::http::ContentType;
 use rocket::tokio;
 use rocket_multipart_form_data::{
-    mime, MultipartFormData, MultipartFormDataField, MultipartFormDataOptions, Repetition,
+    mime, MultipartFormData, MultipartFormDataField, MultipartFormDataOptions, TextField,
 };
 use std::fs;
 use std::path::Path;
+use std::time::{SystemTime, UNIX_EPOCH};
 
 #[get("/")]
 fn index() -> &'static str {
@@ -32,26 +33,56 @@ async fn create(data: Data<'_>, content_type: &ContentType) -> Result<String, st
         .unwrap();
 
     let file_field = form_data.files.get("file");
-    let ext = form_data.texts.remove("ext");
+    let ext_field = form_data.texts.remove("ext");
 
-    if let Some(file_fields) = file_field {
-        let file = &file_fields[0];
+    if let Some(ext_fields) = ext_field {
+        // ! start defining ext ! \\
+        // sorry this is so long
+        let ext = ext_field.unwrap_or(vec![TextField {
+            content_type: None,
+            file_name: None,
+            text: "".to_string(),
+        }])[0]
+            .text;
+        // ! end defining ext ! \\
 
-        let f_content_type = &file.content_type;
-        let f_file_name = &file.file_name;
-        let f_path = &file.path;
+        if let Some(file_fields) = file_field {
+            let file = file_fields[0];
 
-        let salt = thread_rng()
-            .sample_iter(&Alphanumeric)
-            .take(10)
-            .map(char::from)
-            .collect();
-        
+            let f_content_type = file.content_type.unwrap_or("*".parse().unwrap());
+            let f_file_name = file.file_name.unwrap_or("unknown".into());
+            let f_path = file.path;
 
-        let file = tokio::fs::File::create(Path::new(format!("storage/{}.{}", name, ext))).await?;
-        let stream = data.open(512.kibibytes());
+            println!("{}", f_path.display());
 
-        return Ok("".into());
+            let timestamp = SystemTime::now()
+                .duration_since(UNIX_EPOCH)
+                .expect("Time went backwards?")
+                .as_secs();
+            let salt = thread_rng()
+                .sample_iter(&Alphanumeric)
+                .take(10)
+                .map(char::from)
+                .collect();
+            let encoded_name = base64::encode(format!(
+                "{original_name}::{content_type}::{timestamp}::{salt}",
+                original_name = f_file_name[0..10].to_string(),
+                content_type = f_content_type,
+                timestamp = timestamp,
+                salt = salt
+            ));
+
+            let new_file_path = Path::new(format!("storage/{}.{}", &encoded_name, &ext));
+            let file = tokio::fs::File::create(new_file_path).await?;
+            let stream = data.open(512.kibibytes());
+
+            return Ok("".into());
+        } else {
+            return Err(std::io::Error::new(
+                std::io::ErrorKind::Other,
+                "Missing 'file' input field",
+            ));
+        }
     }
 }
 
