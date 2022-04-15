2543eb91a8b13256b5577dbda45bb96ace689f50
matievisthekat <matievisthekat@gmail.com>
2022-04-15 13:30:12 +0200

    add launch function

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,23 @@
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
+use std::fs;
 
 #[get("/")]
 fn index() -> &'static str {
-    "Who's a good boi?"
+    "Hello, world!"
 }
 
-#[launch]
-fn main() {
-    rocket::build().mount("/", routes![index])
+#[rocket::main]
+async fn main() {
+    fs::create_dir_all("storage/").unwrap_or_else(|err| {
+        if err.kind() != std::io::ErrorKind::AlreadyExists {
+            panic!("Failed to create storage directory: {}", err);
+        }
+    });
+
+    rocket::build()
+        .mount("/", routes![index])
+        .launch()
+        .await
+        .unwrap_or_else(|e| panic!("Failed to launch the rocket: {}", e));
 }
