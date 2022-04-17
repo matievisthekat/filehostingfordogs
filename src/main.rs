2d1282ea349898f7036f5daf3e2cea6481e420ba
matievisthekat <matievisthekat@gmail.com>
2022-04-17 19:36:17 +0200

    lots of things ->>
    accept raw files instead of form-data\nshorten base64 encoded file name (remove salt)
    clean up responses

--- src/main.rs
+++ src/main.rs
@@ -1,102 +1,100 @@
 #[macro_use]
 extern crate rocket;
-use rand::distributions::Alphanumeric;
-use rand::{thread_rng, Rng};
-use rocket::data::Data;
-use rocket::http::ContentType;
-use rocket::response::status::{BadRequest, Created};
-use rocket_multipart_form_data::{
-    MultipartFormData, MultipartFormDataField, MultipartFormDataOptions,
-};
+use rocket::data::ToByteUnit;
+use rocket::http::{ContentType, Status};
+use rocket::response::status::Custom;
+use rocket::Data;
 use std::fs;
 use std::path::PathBuf;
 use std::time::{SystemTime, UNIX_EPOCH};
 
 #[get("/")]
 fn index() -> &'static str {
-    "Hello, world!"
+  "Hello, world!"
 }
 
-#[post("/create", data = "<data>")]
+#[post("/create?<ext>&<og_name>", data = "<data>", format = "*/*")]
 async fn create(
-    data: Data<'_>,
-    content_type: &ContentType,
-) -> Result<Created<String>, BadRequest<String>> {
-    let options = MultipartFormDataOptions::with_multipart_form_data_fields(vec![
-        MultipartFormDataField::file("file")
-            .content_type_by_string(Some("*/*"))
-            .unwrap(),
-        MultipartFormDataField::text("ext")
-            .content_type_by_string(Some("text/plain"))
-            .unwrap(),
-    ]);
-    let form_data = MultipartFormData::parse(content_type, data, options).await;
+  data: Option<Data<'_>>,
+  ext: Option<String>,
+  og_name: Option<String>,
+  content_type: &ContentType,
+) -> Result<String, Custom<String>> {
+  let content_type_string = content_type.to_string();
+  let split_string = content_type_string.split(";").collect::<Vec<&str>>();
+  let clean_content_type = split_string.get(0).unwrap();
 
-    match form_data {
-        Ok(mut fd) => {
-            let file_field = fd.files.get("file");
-            let ext_field = fd.texts.remove("ext");
+  if let Some(ext) = ext {
+    if let Some(data) = data {
+      let timestamp = SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .expect("Time went backwards?")
+        .as_secs();
+      let encoded_name = base64::encode_config(
+        format!(
+          "{original_name}::{cnt_type}::{timestamp}",
+          original_name = og_name
+            .unwrap_or("unknown".into())
+            .get(0..7)
+            .unwrap_or("unknown".into()) // this is quite easily the ugliest piece of code i've ever written
+            .to_string(),
+          cnt_type = clean_content_type.to_string(),
+          timestamp = timestamp,
+          // salt = salt
+        ),
+        base64::URL_SAFE_NO_PAD,
+      );
+      let new_file_path = PathBuf::from(format!("storage/{}.{}", &encoded_name, &ext));
+      let file = rocket::tokio::fs::File::create(&new_file_path).await;
 
-            if let Some(ext_fields) = ext_field {
-                let ext = &ext_fields[0].text;
+      if let Ok(file) = file {
+        let res = data.open(5.gigabytes()).stream_to(file).await;
 
-                if let Some(file_fields) = file_field {
-                    let file = &file_fields[0];
-
-                    let f_content_type = &file.content_type;
-                    let f_file_name = &file.file_name;
-                    let f_path = &file.path;
-
-                    let timestamp = SystemTime::now()
-                        .duration_since(UNIX_EPOCH)
-                        .expect("Time went backwards?")
-                        .as_secs();
-                    let salt = thread_rng()
-                        .sample_iter(&Alphanumeric)
-                        .take(10)
-                        .map(char::from)
-                        .collect::<String>();
-                    let encoded_name = base64::encode_config(
-                        format!(
-                            "{original_name}::{cnt_type}::{timestamp}::{salt}",
-                            original_name =
-                                f_file_name.as_ref().unwrap_or(&String::from("unknown"))[0..10]
-                                    .to_string(),
-                            cnt_type = f_content_type.as_ref().unwrap().to_string(), // should default to application/octet-stream
-                            timestamp = timestamp,
-                            salt = salt
-                        ),
-                        base64::URL_SAFE_NO_PAD,
-                    );
-
-                    let new_file_path =
-                        PathBuf::from(format!("storage/{}.{}", &encoded_name, &ext));
-                    fs::copy(f_path, new_file_path)
-                        .expect("Failed to copy file into `storage/` folder");
-
-                    return Ok(Created::new(format!("/f/{}", &encoded_name)));
-                } else {
-                    return Err(BadRequest(Some("Missing 'file' input field".into())));
-                }
-            } else {
-                return Err(BadRequest(Some("Missing 'ext' input field".into())));
-            }
+        if let Ok(_) = res {
+          // return Ok(Created::new(format!("/f/{}", &encoded_name)));
+          return Ok(format!("/f/{}", &encoded_name));
+        } else if let Err(e) = res {
+          return Err(Custom(
+            Status::InternalServerError,
+            format!("Failed to stream data to file: {}", e),
+          ));
+        } else {
+          return Err(Custom(
+            Status::InternalServerError,
+            "Unreachable error was reached. Please report this".to_string(),
+          ));
         }
-        Err(e) => return Err(BadRequest(Some(format!("{}", e)))),
+      } else {
+        return Err(Custom(
+          rocket::http::Status::InternalServerError,
+          "Could not create file".into(),
+        ));
+      }
+    } else {
+      return Err(Custom(
+        Status::BadRequest,
+        "Missing 'file' input field".into(),
+      ));
     }
+  } else {
+    return Err(Custom(
+      Status::BadRequest,
+      "Missing '?ext=<file_extension>' query param".into(),
+    ));
+  }
 }
 
 #[rocket::main]
 async fn main() {
-    fs::create_dir_all("storage/").unwrap_or_else(|err| {
-        if err.kind() != std::io::ErrorKind::AlreadyExists {
-            panic!("Failed to create storage directory: {}", err);
-        }
-    });
+  fs::create_dir_all("storage/").unwrap_or_else(|err| {
+    if err.kind() != std::io::ErrorKind::AlreadyExists {
+      panic!("Failed to create storage directory: {}", err);
+    }
+  });
 
-    rocket::build()
-        .mount("/", routes![index, create])
-        .launch()
-        .await
-        .unwrap_or_else(|e| panic!("Failed to launch the rocket: {}", e));
+  rocket::build()
+    .mount("/", routes![index, create])
+    .launch()
+    .await
+    .unwrap_or_else(|e| panic!("Failed to launch the rocket: {}", e));
 }
