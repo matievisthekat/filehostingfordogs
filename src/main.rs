c24eb7228cc1a70386bd4607b8ae3393b4bc424a
matievisthekat <matievisthekat@gmail.com>
2022-04-17 11:40:02 +0200

    handle failure to parse form data

--- src/main.rs
+++ src/main.rs
@@ -30,53 +30,59 @@ async fn create(
             .content_type_by_string(Some("text/plain"))
             .unwrap(),
     ]);
-    let mut form_data = MultipartFormData::parse(content_type, data, options)
-        .await
-        .unwrap_or_else(|e| panic!("{}", e));
+    let form_data = MultipartFormData::parse(content_type, data, options).await;
 
-    let file_field = form_data.files.get("file");
-    let ext_field = form_data.texts.remove("ext");
+    match form_data {
+        Ok(mut fd) => {
+            let file_field = fd.files.get("file");
+            let ext_field = fd.texts.remove("ext");
 
-    if let Some(ext_fields) = ext_field {
-        let ext = &ext_fields[0].text;
+            if let Some(ext_fields) = ext_field {
+                let ext = &ext_fields[0].text;
 
-        if let Some(file_fields) = file_field {
-            let file = &file_fields[0];
+                if let Some(file_fields) = file_field {
+                    let file = &file_fields[0];
 
-            let f_content_type = &file.content_type;
-            let f_file_name = &file.file_name;
-            let f_path = &file.path;
+                    let f_content_type = &file.content_type;
+                    let f_file_name = &file.file_name;
+                    let f_path = &file.path;
 
-            let timestamp = SystemTime::now()
-                .duration_since(UNIX_EPOCH)
-                .expect("Time went backwards?")
-                .as_secs();
-            let salt = thread_rng()
-                .sample_iter(&Alphanumeric)
-                .take(10)
-                .map(char::from)
-                .collect::<String>();
-            let encoded_name = base64::encode_config(
-                format!(
-                    "{original_name}::{cnt_type}::{timestamp}::{salt}",
-                    original_name =
-                        f_file_name.as_ref().unwrap_or(&String::from("unknown"))[0..10].to_string(),
-                    cnt_type = f_content_type.as_ref().unwrap().to_string(), // should default to application/octet-stream
-                    timestamp = timestamp,
-                    salt = salt
-                ),
-                base64::URL_SAFE_NO_PAD,
-            );
+                    let timestamp = SystemTime::now()
+                        .duration_since(UNIX_EPOCH)
+                        .expect("Time went backwards?")
+                        .as_secs();
+                    let salt = thread_rng()
+                        .sample_iter(&Alphanumeric)
+                        .take(10)
+                        .map(char::from)
+                        .collect::<String>();
+                    let encoded_name = base64::encode_config(
+                        format!(
+                            "{original_name}::{cnt_type}::{timestamp}::{salt}",
+                            original_name =
+                                f_file_name.as_ref().unwrap_or(&String::from("unknown"))[0..10]
+                                    .to_string(),
+                            cnt_type = f_content_type.as_ref().unwrap().to_string(), // should default to application/octet-stream
+                            timestamp = timestamp,
+                            salt = salt
+                        ),
+                        base64::URL_SAFE_NO_PAD,
+                    );
 
-            let new_file_path = PathBuf::from(format!("storage/{}.{}", &encoded_name, &ext));
-            fs::copy(f_path, new_file_path).expect("Failed to copy file into `storage/` folder");
+                    let new_file_path =
+                        PathBuf::from(format!("storage/{}.{}", &encoded_name, &ext));
+                    fs::copy(f_path, new_file_path)
+                        .expect("Failed to copy file into `storage/` folder");
 
-            return Ok(Created::new(format!("/f/{}", &encoded_name)));
-        } else {
-            return Err(BadRequest(Some("Missing 'file' input field".into())));
+                    return Ok(Created::new(format!("/f/{}", &encoded_name)));
+                } else {
+                    return Err(BadRequest(Some("Missing 'file' input field".into())));
+                }
+            } else {
+                return Err(BadRequest(Some("Missing 'ext' input field".into())));
+            }
         }
-    } else {
-        return Err(BadRequest(Some("Missing 'ext' input field".into())));
+        Err(e) => return Err(BadRequest(Some(format!("{}", e)))),
     }
 }
 
